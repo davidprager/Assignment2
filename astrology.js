662aaf701dfef3fcb87ad8491b04490b11992b20
davidprager <david_prager@yahoo.com>
2022-03-16 18:47:04 -0400

    Initial commit

---
+++ astrology.js
@@ -0,0 +1,13 @@
+/* This file contains a program to determine a user's astrological sign
+*/
+
+"use strict"
+
+
+/* This program will display a user's astrological sign based upon a date provided in the HTML */
+function displaySign(inputDate) {
+    // convert input to a number
+    let date = parseInt(inputDate);
+
+
+}
---
+++ patterns.js
@@ -0,0 +1,20 @@
+/* This file contains functions that produce patterns
+*/
+
+"use strict"
+
+/* This function will display different patterns based upon the provided size */
+function displayPatterns(size) {
+
+}
+
+
+/* This function makes sure that the size is in the valid range */
+function validateInput(size) {
+
+}
+
+/* This function returns a symbol based upon a random number */
+function getSymbol() {
+
+}
